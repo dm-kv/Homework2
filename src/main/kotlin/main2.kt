8d0c3f402453ad62d4819d8ea25c14a663150595
Dm Kv <demon1st@yandex.ru>
2026-01-14 00:10:09 +0300

    corrected task 2

--- src/main/kotlin/main2.kt
+++ src/main/kotlin/main2.kt
@@ -1,5 +1,5 @@
 fun main() {
     val likes = 8000
     val s = if (likes % 10 != 1 || likes % 100 == 11) " людям" else " человеку"
-    println("Понравилось " + likes + s)
+    println("Понравилось $likes$s ")
 }
